9d704f292f88592efc92cbc3b9fc70ff4e5dd78b
ihsan-guc <ihsanguc.33@gmail.com>
2022-01-21 14:34:10 +0300

    Memory Cache Add.

---
+++ TodoAppOne/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using TodoAppOne.Model;
+
+namespace TodoAppOne.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        IMemoryCache _memoryCache;
+        const string usersKey = "users";
+
+        public UserController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        [Route("GetUserList")]
+        public IActionResult GetUserList()
+        {
+            if (_memoryCache.TryGetValue(usersKey, out object list))
+                return Ok(list);
+            return null;
+        }
+        [HttpPost]
+        [Route("AddUser")]
+        public IActionResult AddUser(UserDTO userDTO)
+        {
+            _memoryCache.Set(usersKey, userDTO, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(20),
+                Priority = CacheItemPriority.Normal
+            });
+            return Ok(userDTO);
+        }
+        [HttpPost]
+        [Route("RemoveUser")]
+        public IActionResult RemoveUser(UserDTO userDTO)
+        {
+            _memoryCache.Remove(usersKey);
+            return Ok();
+        }
+    }
+}
---
+++ TodoAppOne/Model/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAppOne.Model
+{
+    public class UserDTO
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Description { get; set; }
+    }
+}
